87802f24e6f513b62b0e2e5fecde99960f5d5b59
Chlaudya Mamuaya <mamuaya.chlaudya@gmail.com>
2020-08-18 23:41:43 +0800

    deploying we

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-// import Router from "./router/router";
 import "./App.css";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
@@ -9,13 +7,11 @@ import Sayembara from "./components/PorfolioOne";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <React.Fragment>
-          <NavBar />
-          <Home id="home" />
-          <Sayembara id="sayembara" />
-        </React.Fragment>
-      </Router>
+      <React.Fragment>
+        <NavBar />
+        <Home id="home" />
+        <Sayembara id="sayembara" />
+      </React.Fragment>
     </div>
   );
 }
